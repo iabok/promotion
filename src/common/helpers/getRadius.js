18a3bbf1a3fc30bec815756bf9eb3f75fd6f4ece
Abok Isaac <abokisaac@gmail.com>
2018-06-11 21:34:01 +0300

    Refactored getRadius to use the in built loop Geopoint

--- src/common/helpers/getRadius.js
+++ src/common/helpers/getRadius.js
@@ -1,4 +1,5 @@
 'use strict';
+var loopback = require('loopback');
 module.exports = {
     getRadius: function(points) {
         var latFrom = points.from.latitude;
@@ -6,24 +7,11 @@ module.exports = {
         var latTo = points.to.latitude;
         var longTo = points.to.longtitude;
 
-        var distance = this.getDistanceFromLatLonInKm(latFrom, longFrom, latTo, longTo);
+        var from = new loopback.GeoPoint( {lat: latFrom, lng: longFrom} );
+        var to = new loopback.GeoPoint( {lat: latTo, lng: longTo} );
+
+        var distance = from.distanceTo( to, {type: 'kilometers'});
 
         return distance / 2;
-    },
-    getDistanceFromLatLonInKm: function (lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = this.deg2rad(lat2-lat1);
-        var dLon = this.deg2rad(lon2-lon1);
-        var a =
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c; // Distance in km
-
-        return d;
-    },
-    deg2rad: function(deg) {
-        return deg * (Math.PI/180);
     }
 };
